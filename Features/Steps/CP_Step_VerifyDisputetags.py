2659a5988b24ee766f934b4a944bc98e70622f04
Mariyam Shahid <merriam.shahid@gmail.com>
2023-03-29 11:16:41 +0500

    verify dispute tag

---
+++ Features/Steps/CP_Step_VerifyDisputetags.py
@@ -0,0 +1,19 @@
+import time
+from behave import *
+
+
+from behave import *
+
+
+
+
+@when("customer at dashboard and hover over side panel and click invoice option")
+def hovering_sidepanel(context):
+        context.cadency.Verify_DisputeTag.hovering()
+        time.sleep(2)
+
+
+@then("search dispute tagged invoice and save the catched inv number")
+def step_impl(context):
+        context.cadency.Verify_DisputeTag.catch_tag()
+        time.sleep(6)
