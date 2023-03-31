d5350dffa092c582b8a35a43a1d9d3db6c44519d
farrukhalvi123 <59420418+farrukhalvi123@users.noreply.github.com>
2023-03-31 13:07:57 +0500

    Normal Invoices Changes

--- Features/Steps/Commons.py
+++ Features/Steps/Commons.py
@@ -9,12 +9,15 @@ import time
 
 from Constants.URLS import TestData
 from Pages.Admin_LoginPage import AdminLoginPage
+from Pages.AdvPaySetPage import AdvancePaymentSetting
+from Pages.Creditnotepages import Cns
 from Pages.CustomerPages import CustomerPages
 from Pages.LoginPage import LoginPage
 from Pages.InvoicePage import InvoicePage
 from Pages.Admin_ForgotPass import AdminForgotPass
 from Pages.Admin_AddNewUser import AdminAddUsers
 from Pages.Manage_Teams import ManageTeams
+from Pages.PormisetoPaypsges import Ptop
 from Pages.TemplatePages import TemplatePage
 from Pages.Verify_Modules_Permissions import VerifyPermissions
 
@@ -35,6 +38,6 @@ class cadencyweb:
         self.admin_add_users = AdminAddUsers(driver)
         self.admin_manage_teams = ManageTeams(driver)
         self.admin_verify_permissions = VerifyPermissions(driver)
-        self.admin_onboarding_merchant = OnboardingMerchant(driver)
+        # self.admin_onboarding_merchant = OnboardingMerchant(driver)
         self.PromisetoPay=Ptop(driver)
-        self.PromisetoPay = Ptop(driver)
+        # self.PromisetoPay = Ptop(driver)
